933fcec704d8e49a72bd48669a3d7114d50c2c15
Said Houwasse <said.houwasse@gmail.com>
2018-12-13 20:02:47 -0500

    Creating post, get route and models

---
+++ Quiz-Backend/Controllers/QuestionsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quiz_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+
+
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Models.Question> Get()
+        {
+            return new Models.Question[] {
+                 new Models.Question { Text = "Hello 1"},
+                 new Models.Question { Text = "Hello 2"},
+                 new Models.Question { Text = "Hello 3"}
+            };
+        }
+
+
+        // POST api/questions
+        [HttpPost]
+        public void Post([FromBody] Models.Question  question)
+        {
+            
+        }
+    }
+}
---
+++ Quiz-Backend/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz_Backend.Models
+{
+    public class Question
+    {
+        public string Text { get; set; }
+    }
+}
